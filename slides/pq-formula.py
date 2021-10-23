6e811a0bf6d845121a6cf359d63763f4de4f1ae3
nicholas-roether <nicholas.roether@t-online.de>
2021-10-23 17:55:41 +0200

    Finish PQ formula stuff

--- slides/pq-formula.py
+++ slides/pq-formula.py
@@ -1,18 +1,19 @@
 from manim import *
-from manim_presentation import *;
 
-class PQFormula(Slide):
+
+class PQFormula(Scene):
+	sectionCounter = 0
+	sectionStartTime = 0
+
+
 	def construct(self):
-		# pq_formula = MathTex(r"$ -{{p}} \over {2} $ \pm \sqrt{ $ {{p}}^2 \over {4} $ - {{q}} }")
-		# pq_formula = MathTex(r"{- {{p}} \over 2} \pm \sqrt { p^2 \over 2 } - q")
 		pq_formula = MathTex("{-", "p", "\\over 2} \\pm", "\\sqrt{", "{p", "^2 \\over 2} -", "q}")
 
-		# Show the formula
+		# Show the formula (duration: 1.5)
 		self.play(Write(pq_formula))
-		self.pause()
-		
+		self.wait(0.5)
 
-		# Show it's meaning
+		# Show it's meaning (duration: 3.5)
 		self.play(pq_formula.animate.shift(DOWN))
 
 		pq_equation = MathTex("x^2 + {{p}} x + {{q}} = 0")
@@ -24,67 +25,73 @@ class PQFormula(Slide):
 			pq_formula[1].animate.set_color(RED),
 			pq_formula[4].animate.set_color(RED),
 			pq_formula[6].animate.set_color(BLUE)
-			# pq_formula.animate.set_color_by_tex("q", BLUE)
 		)
 
 		self.play(pq_formula.animate.shift(RIGHT), run_time=0.5)
 
 		implies_x_equals = MathTex("\\Rightarrow x =")
 		implies_x_equals.next_to(pq_formula, LEFT)
-		self.play(Write(implies_x_equals), run_time=0.2)
-		self.pause()
+		self.play(Write(implies_x_equals), run_time=0.5)
+		self.wait(0.5)
 
-		# Move to box
+		# Move to box (duration: 2.5)
 		self.play(
 			Unwrite(pq_equation),
-			Unwrite(implies_x_equals)
+			Unwrite(implies_x_equals),
+			run_time=0.5
 		)
-		self.play(pq_formula.animate.align_on_border(LEFT + UP))
-		formula_frame = SurroundingRectangle(pq_formula, buff=MED_LARGE_BUFF)
+		self.play(pq_formula.animate.align_on_border(LEFT + UP, buff=SMALL_BUFF))
+		formula_frame = SurroundingRectangle(pq_formula, buff=SMALL_BUFF)
 		formula_frame.set_color(DARK_BLUE)
-		self.play(Create(formula_frame))
-		self.pause()
+		self.play(Create(formula_frame), run_time=0.5)
+		self.wait(0.5)
 
-		# Create example
-		example = MathTex("x^2 +", "2", "x +", "4")
-		example.shift(UP)
+		# Create example (duration: 1.5)
+		example = MathTex("x^2 +", "2", "x +", "4", "= 0")
+		example.shift(UP + RIGHT)
 		example[1].set_color(RED)
 		example[3].set_color(BLUE)
 		self.play(Write(example))
-		self.pause()
+		self.wait(0.5)
 
-		# Show solution attempt
+		# Show plugging in values (duration: 2)
 		solution_attempt_1 = MathTex("\\Rightarrow x = {-", "2", "\\over 2}", "\\pm", "\\sqrt{", "{2", "^2 \\over 2}", "-", "4}")
+		solution_attempt_1.shift(RIGHT)
 		solution_attempt_1[1].set_color(RED)
 		solution_attempt_1[5].set_color(RED)
 		solution_attempt_1[8].set_color(BLUE)
 		self.play(Write(solution_attempt_1))
+		self.wait(0.5)
 
-
+		# Show solution attempt (duration: 2)
 		solution_attempt_2 = MathTex("\\Rightarrow x = {-", "1", "\\pm", "\\sqrt{", "{4 \\over 2}", "-", "4}")
-		self.play(ReplacementTransform(solution_attempt_1, solution_attempt_2))
+		solution_attempt_2.shift(RIGHT)
+		self.play(ReplacementTransform(solution_attempt_1, solution_attempt_2), run_time=0.5)
 
 		solution_attempt_3 = MathTex("\\Rightarrow x = {-", "1", "\\pm", "\\sqrt{", "2", "-", "4}")
-		self.play(ReplacementTransform(solution_attempt_2, solution_attempt_3))
+		solution_attempt_3.shift(RIGHT)
+		self.play(ReplacementTransform(solution_attempt_2, solution_attempt_3), run_time=0.5)
 
 		solution_attempt_4 = MathTex("\\Rightarrow x = {-", "1", "\\pm", "\\sqrt{", "-2", "}")
-		self.play(ReplacementTransform(solution_attempt_3, solution_attempt_4))
-		self.pause()
+		solution_attempt_4.shift(RIGHT)
+		self.play(ReplacementTransform(solution_attempt_3, solution_attempt_4), run_time=0.5)
+		self.wait(0.5)
 
-		# Show that no solution exists
-		self.play(
-			solution_attempt_4.animate.align_on_border(RIGHT + UP),
-			Unwrite(example)
-		)
+		# Show that no solution exists (duration: till end)
+		self.play(Unwrite(example), run_time=0.5)
+		self.play(solution_attempt_4.animate.align_on_border(LEFT + DOWN, buff=SMALL_BUFF))
 
 		axes = Axes(
 			x_range=[-10, 10, 1],
 			y_range=[-10, 10, 1],
-			x_length=10,
+			x_length=5,
+			y_length=3.8
 		)
-		axes_labels = axes.get_axis_labels()
+		axes.shift(RIGHT * 2)
+		axis_labels = axes.get_axis_labels()
 		graph = axes.get_graph(lambda x: x**2 + 2 * x + 4, color=BLUE)
-		self.play(Create(axes), Write(axes_labels))
-		self.play(DrawBorderThenFill(graph))
+		self.play(Create(axes), Write(axis_labels), run_time=1)
+		self.play(Create(graph))
+		self.wait(0.5)
 
-		self.wait()
+		
